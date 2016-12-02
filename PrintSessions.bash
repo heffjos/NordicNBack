9ab7049af7f7f65326628663bb66151f6444284a
Joe Heffernan <jheffernan@mcw.edu>
2016-12-02 17:37:40 -0600

    Added bash script to print sessions and the printed sessions as xml files.

---
+++ PrintSessions.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Run=$1
+DesignFile=/home/heffjos/Documents/Tasks/Nordic/NBack/Design.csv
+
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo "<!-- Must be a top level tag to include all tags to include -->"
+echo "<IncludeSessions>"
+echo "    <Session>"
+echo "        <name>NBack</name>"
+
+for Line in `grep -E ^${Run} ${DesignFile}`
+do
+    TrialType=`echo ${Line} | awk -F, '{print $3}'`
+    Letter=`echo ${Line} | awk -F, '{print $5}'`
+
+    if [[ ${TrialType} != Rest ]]
+    then
+        echo "        <runtrial>${TrialType}_${Letter}</runtrial>"
+    else
+        echo "        <runtrial>Rest</runtrial>"
+    fi
+done
+
+echo "    </Session>"
+echo "</IncludeSessions>"
