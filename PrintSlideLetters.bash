72c5744af1637093d9fc073c057c5d157d1cf00a
Joe Heffernan <jheffernan@mcw.edu>
2016-12-02 17:36:13 -0600

    Added bash script to print slides and the printed slides as xml file.

---
+++ PrintSlideLetters.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for oneLetter in G K F C B M Q X V R S T H J Z D
+do
+    echo "    <Slide>"
+    echo "        <name>Letter_${oneLetter}</name>"
+    echo "        <text>${oneLetter}</text>"
+    echo "        <font>Arial</font>"
+    echo "        <fontsize>72</fontsize>"
+    echo "        <duration>500</duration>"
+    echo "        <expectedResponse>"
+    echo "            <key>C</key>"
+    echo "            <count>1</count>"
+    echo "        </epxectedResponse>"
+    echo "    </Slide>"
+    echo
+done
